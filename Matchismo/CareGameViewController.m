8f4a57d33577ea1917b051c8648d1c2c41a62243
Yourtion <yourtion@gmail.com>
2014-01-18 19:03:28 +0800

    finish card

--- Matchismo/CareGameViewController.m
+++ Matchismo/CareGameViewController.m
@@ -26,4 +26,9 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)CardBtn:(UIButton *)sender {
+    [sender setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
+    [sender setTitle:@"" forState:UIControlStateNormal];
+}
+
 @end
