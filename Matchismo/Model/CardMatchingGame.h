b19c336ac27e680cb55879170e628470a0063fc7
Yourtion <yourtion@gmail.com>
2014-01-19 01:55:26 +0800

    Add Game Model

---
+++ Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,23 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Yourtion on 14-1-18.
+//  Copyright (c) 2014年 Yourtion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Card.h"
+#import "Deck.h"
+
+@interface CardMatchingGame : NSObject
+
+-(instancetype)initWithCardCount:(NSUInteger)count
+                       usingDeck:(Deck *)deck;
+
+-(void)chooseCardAtIndex:(NSUInteger)index;
+-(Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic, readonly)NSInteger score;
+
+@end
