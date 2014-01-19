8b9242a9810425c98e0a22d954d22b4dc2881d7c
Yourtion <yourtion@gmail.com>
2014-01-19 14:30:08 +0800

    use id and isKindOfClass to valid PlayingCard

--- Matchismo/Model/PlayingCard.m
+++ Matchismo/Model/PlayingCard.m
@@ -13,11 +13,14 @@
 {
     int score = 0;
     if ([otherCards count] == 1) {
-        PlayingCard *otherCard = [otherCards firstObject];
-        if (otherCard.rank == self.rank) {
-            score = 4;
-        }else if ([otherCard.suit isEqualToString:self.suit]){
-            score = 1;
+        id card = [otherCards firstObject];
+        if ([card isKindOfClass:[PlayingCard class]]) {
+            PlayingCard *otherCard = (PlayingCard *)card;
+            if (otherCard.rank == self.rank) {
+                score = 4;
+            }else if ([otherCard.suit isEqualToString:self.suit]){
+                score = 1;
+            }
         }
     }
     return score;
